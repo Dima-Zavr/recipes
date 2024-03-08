dc77bdf22e41a2b4ab9fdf2c08645815b9a203b0
Sosin Dima <sosindima5@gmail.com>
2024-03-08 22:30:11 +0700

    -

--- src/api/request.js
+++
@@ -1,7 +0,0 @@
-export const baseUrl = "https://shift-backend.onrender.com"
-
-export const request = {
-  fetch: function (path, params) {
-    return fetch(baseUrl + path)
-  },
-}
--- src/components/button/index.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./styles.module.css"
-
-export const Button = ({ children, onClick, type }) => (
-  <button type={type} className={styles.button} onClick={onClick}>
-    {children}
-  </button>
-)
