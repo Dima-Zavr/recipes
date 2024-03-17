7ef99e07b2d03e963dfc0a4afcedeaf9d99a13f3
Sosin Dima <sosindima5@gmail.com>
2024-03-17 18:59:29 +0700

    -

--- src/components/btn/index.jsx
+++
@@ -1,7 +0,0 @@
-import { MyButton } from "./styled_components"
-
-export const Button = ({ children, onClick, type }) => (
-  <MyButton type={type} onClick={onClick}>
-    {children}
-  </MyButton>
-)
